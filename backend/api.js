1b14fd684d85e1f2d286cd68658ce09be51b1dde
Christopher Renfro <ctrenfro22@gmail.com>
2022-06-29 14:30:16 -0400

    url change

--- backend/api.js
+++ backend/api.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(cors());
 
+const path = require("path");
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "../build")));
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../build/index.html"));
+});
+
 app.get("/new", (req, res) => {
   const engine = req.query.engine;
   const prompt = req.query.prompt;
