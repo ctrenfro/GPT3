0bbd4fb1b24bd131f7cbcac326abff9ab699abf1
Christopher Renfro <ctrenfro22@gmail.com>
2022-07-05 15:17:13 -0400

    change

--- api.js
+++ api.js
@@ -32,7 +32,6 @@ app.get("/new", (req, res) => {
   const options = {
     method: "POST",
     url: `https://api.openai.com/v1/engines/${engine}/completions`,
-    httpsAgent: agent,
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
