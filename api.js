cd7dba815271ae63fd85384e6e73bb065ff76d69
Christopher Renfro <ctrenfro22@gmail.com>
2022-07-05 15:22:10 -0400

    changes

--- api.js
+++ api.js
@@ -1,4 +1,3 @@
-import https from "node-https";
 const port = process.env.PORT || 8000;
 const express = require("express");
 const cors = require("cors");
@@ -17,10 +16,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(cors());
-
-const agent = new https.Agent({ rejectUnauthorized: false });
-
-//const httpsAgent = new https.Agent({ ca: MY_CA_BUNDLE });
 
 app.get("/new", (req, res) => {
   const engine = req.query.engine;
